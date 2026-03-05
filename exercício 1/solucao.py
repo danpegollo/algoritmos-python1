09c05bced2f6b021316c7ab1f6c1531e9905f14b
danpegollo <danielpegollo29@gmail.com>
2026-03-05 16:35:15 -0300

    Adiciona o exercício 1

--- exercício 1/solucao.py
+++ exercício 1/solucao.py
@@ -1,4 +1,4 @@
 distancia = float(input("Digite a distância em km: "))
 tempo = float(input("Digite o tempo de viagem em horas: "))
-v = distancia/tempo
-print("Sua velociade é: ", v, " km/h")
+velocidade = distancia/tempo
+print("Sua velociade é: ", velocidade, " km/h")
